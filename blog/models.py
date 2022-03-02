894fa39831331ec19c1918d46fd516c35d4f1338
hhhyeong <yommyhy77@gmail.com>
2022-03-02 19:59:37 +0900

    0302 Post모델 생성하고 시간 설정하기

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # author
+    
+    # admin 패널 Post 페이지에서 model Object명 대신 title이 보이도록 하기.
+    def __str__(self):
+        return f'[{self.pk}] {self.title}'
